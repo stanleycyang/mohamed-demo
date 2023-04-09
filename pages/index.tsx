5dbe4b23380577d0b33077104f8522faacbd6d9b
Stanley Yang <me@stanleycyang.com>
2023-04-08 22:53:55 -0700

    Mohamed's changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -71,39 +71,29 @@ const Home: NextPage = () => {
   };
 
   return (
-    <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
+    <div className='flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen'>
       <Head>
         <title>Twitter Bio Generator</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <Header />
-      <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20">
-        <a
-          className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-sm text-gray-600 shadow-md transition-colors hover:bg-gray-100 mb-5"
-          href="https://github.com/Nutlope/twitterbio"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Github />
-          <p>Star on GitHub</p>
-        </a>
-        <h1 className="sm:text-6xl text-4xl max-w-[708px] font-bold text-slate-900">
-          Generate your next Twitter bio using chatGPT
+      <main className='flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20'>
+        <h1 className='sm:text-6xl text-4xl max-w-[708px] font-bold text-slate-900'>
+          ModernMuslim Rocks
         </h1>
-        <p className="text-slate-500 mt-5">47,118 bios generated so far.</p>
-        <div className="max-w-xl w-full">
-          <div className="flex mt-10 items-center space-x-3">
+        <div className='max-w-xl w-full'>
+          <div className='flex mt-10 items-center space-x-3'>
             <Image
-              src="/1-black.png"
+              src='/1-black.png'
               width={30}
               height={30}
-              alt="1 icon"
-              className="mb-5 sm:mb-0"
+              alt='1 icon'
+              className='mb-5 sm:mb-0'
             />
-            <p className="text-left font-medium">
+            <p className='text-left font-medium'>
               Copy your current bio{" "}
-              <span className="text-slate-500">
+              <span className='text-slate-500'>
                 (or write a few sentences about yourself)
               </span>
               .
@@ -113,22 +103,22 @@ const Home: NextPage = () => {
             value={bio}
             onChange={(e) => setBio(e.target.value)}
             rows={4}
-            className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
+            className='w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5'
             placeholder={
               "e.g. Senior Developer Advocate @vercel. Tweeting about web development, AI, and React / Next.js. Writing nutlope.substack.com."
             }
           />
-          <div className="flex mb-5 items-center space-x-3">
-            <Image src="/2-black.png" width={30} height={30} alt="1 icon" />
-            <p className="text-left font-medium">Select your vibe.</p>
+          <div className='flex mb-5 items-center space-x-3'>
+            <Image src='/2-black.png' width={30} height={30} alt='1 icon' />
+            <p className='text-left font-medium'>Select your vibe.</p>
           </div>
-          <div className="block">
+          <div className='block'>
             <DropDown vibe={vibe} setVibe={(newVibe) => setVibe(newVibe)} />
           </div>
 
           {!loading && (
             <button
-              className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
+              className='bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full'
               onClick={(e) => generateBio(e)}
             >
               Generate your bio &rarr;
@@ -136,38 +126,38 @@ const Home: NextPage = () => {
           )}
           {loading && (
             <button
-              className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
+              className='bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full'
               disabled
             >
-              <LoadingDots color="white" style="large" />
+              <LoadingDots color='white' style='large' />
             </button>
           )}
         </div>
         <Toaster
-          position="top-center"
+          position='top-center'
           reverseOrder={false}
           toastOptions={{ duration: 2000 }}
         />
-        <hr className="h-px bg-gray-700 border-1 dark:bg-gray-700" />
-        <div className="space-y-10 my-10">
+        <hr className='h-px bg-gray-700 border-1 dark:bg-gray-700' />
+        <div className='space-y-10 my-10'>
           {generatedBios && (
             <>
               <div>
                 <h2
-                  className="sm:text-4xl text-3xl font-bold text-slate-900 mx-auto"
+                  className='sm:text-4xl text-3xl font-bold text-slate-900 mx-auto'
                   ref={bioRef}
                 >
                   Your generated bios
                 </h2>
               </div>
-              <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
+              <div className='space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto'>
                 {generatedBios
                   .substring(generatedBios.indexOf("1") + 3)
                   .split("2.")
                   .map((generatedBio) => {
                     return (
                       <div
-                        className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border"
+                        className='bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border'
                         onClick={() => {
                           navigator.clipboard.writeText(generatedBio);
                           toast("Bio copied to clipboard", {
